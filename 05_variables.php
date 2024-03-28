49dfc50d1446668f6c4ce0e8b0ec6d642fca7eb0
Rahul Aggarwal <rahulagg217@gmail.com>
2024-03-28 10:22:17 +0530

    variables

--- 05_variables.php
+++ 05_variables.php
@@ -1,9 +1,16 @@
 <?php
-// echo "This is lecture 5";
+//php is a losely typed language
+// echo "this is tutorial 5";
 // variables are containers for storing information
-// Starts with a $
-$name = "Rahul";
-$income = "1B$";
+// Starts with $
 
-echo "This guy is $name"
-?>-
+$name = "Rahul";
+$income = 1000000000000000000;
+
+// variables are case sensitive $namE is different from $name .
+
+echo "this guy is $name and his income is $income<br>";
+
+echo "$name is a good boy<br>";
+echo "$name is an engineer<br>";
+?>
